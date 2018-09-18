9797ecec35d2b1b0c6356b8ba7b8dc3645bda502
ryan <ryanj.belke@gmail.com>
2018-09-18 18:00:51 -0500

    reactstrap, babel plugins and css import into _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <div>Hello</div>
+import React from 'react'
+import { Button } from 'reactstrap'
+import { Alert } from 'reactstrap'
+
+export default class Test extends React.Component {
+  render() {
+    return(
+      <div>
+        <Alert color="primary">Hello Project is strapi-next with Bootstrap</Alert>
+        &nbsp; <Button color="primary">Hello from nextjs</Button>
+      </div>
+    )
+  }
+}
